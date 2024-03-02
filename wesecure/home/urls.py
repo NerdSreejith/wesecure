4743f066082d04b04a0c8f1f32ce7e8180c735f9
Sreejith C <sree@kali.kali>
2024-03-02 19:55:32 +0530

    template inherites

--- wesecure/home/urls.py
+++ wesecure/home/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path,include
 from . import  views
 urlpatterns = [
-    path('',views.home,name="home")
+    path('',views.home,name="home"),
+    path('about/',views.about_us,name="about"),
+    path('service/',views.service,name="service"),
+    path('team/',views.team,name="team"),
+    path('why/',views.why,name="why"),
+    path('example/',views.example,name="example")
+
 ]
--- wesecure/home/views.py
+++ wesecure/home/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def  about_us(request):
+    return  render(request,'about.html')
+
+def service(request):
+    return render(request,"service.html")
+
+def team(request):
+    return render(request,"team.html")
+
+def why(request):
+    return  render(request,"why.html")
+
+
+def example(request):
+    return render(request,"example.html")
